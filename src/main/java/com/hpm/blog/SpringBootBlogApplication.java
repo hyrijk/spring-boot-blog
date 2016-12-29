05a7d702651d5901b9921f7ac2a9993576f76d64
peiming <hyrijk@gmail.com>
2016-12-29 22:51:03 +0800

    init

---
+++ src/main/java/com/hpm/blog/SpringBootBlogApplication.java
@@ -0,0 +1,12 @@
+package com.hpm.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootBlogApplication.class, args);
+	}
+}
