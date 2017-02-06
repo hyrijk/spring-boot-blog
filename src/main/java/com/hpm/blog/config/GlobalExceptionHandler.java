8b70983e846fdd56a455634d163ebf25c6bd682f
peiming <hyrijk@gmail.com>
2017-02-06 20:12:17 +0800

    全局异常处理

---
+++ src/main/java/com/hpm/blog/config/GlobalExceptionHandler.java
@@ -0,0 +1,30 @@
+package com.hpm.blog.config;
+
+
+import com.alibaba.fastjson.JSONObject;
+import org.apache.commons.lang.exception.ExceptionUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.ServletRequest;
+
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    private Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+    @ResponseBody
+    @ExceptionHandler(Exception.class)
+    public Object handleException(Exception e) {
+        logger.error(ExceptionUtils.getFullStackTrace(e));  // 记录错误信息
+        String msg = e.getMessage();
+        if (msg == null || msg.equals("")) {
+            msg = "服务器出错";
+        }
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("message", msg);
+        return jsonObject;
+    }
+}
